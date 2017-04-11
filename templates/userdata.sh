c957450cd3b14ce2646ebef467d18317e04065de
Filipe Niero Felisbino <filipenf@gmail.com>
2017-04-11 11:42:19 -0300

    Add initial userdata

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-#TODO: add userdata
+#TODO: make the script OS-aware (debian/ubuntu/arch/etc)
+yum -y install python-crypto python-paramiko python-yaml python-jinja2 python-simplejson python-pip git
+pip install --upgrade pip
+pip install ansible
+
+echo > /etc/ec2_conf.yml <<EOF
+{% ec2_conf | to_yaml %}
+EOF
+
+ssh-keyscan {{ deploy_host }} >> ~/.ssh/known_hosts
+{% if deploy_key is defined %}
+echo "{{ deploy_key }}" >> ~/.ssh/deploy_key
+{% endif %}
+repo_url="{% if deploy_username is defined %}{{ deploy_username }}@{%- endif %}{{ deploy_host }}:{{ deploy_repo }}"
+
+ansible-pull -i localhost, -c local --accept-host-key \
+    -C {{ deploy_branch | default('master') }} \
+    -U "$repo_url" deploy/deploy.yml \
+    -e target=localhost -e @/etc/ec2_conf.yml
+
